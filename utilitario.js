84ab9ec95466b9457f8ae59c665a1faf0d980457
luisjimenezgx@gmai.com <luisjimenezgx@gmail.com>
2025-09-23 14:20:59 -0400

    Cree la funcion Recuperar Texto

---
+++ utilitario.js
@@ -0,0 +1,6 @@
+recuperarTexto = function(idComponente){
+    let valorIngresado;
+    let componente = document.getElementById(idComponente);
+    valorIngresado = componente.value;
+    return valorIngresado;
+}
