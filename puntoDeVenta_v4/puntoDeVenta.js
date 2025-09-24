5db45a5d2a48749aa4c8b0617cf1e164d00103f7
luisjimenezgx@gmai.com <luisjimenezgx@gmail.com>
2025-09-24 17:22:58 -0400

    Se muestra Resumen segun especificaciones

--- puntoDeVenta_v4/puntoDeVenta.js
+++ puntoDeVenta_v4/puntoDeVenta.js
@@ -15,14 +15,8 @@ calcularValorTotal = function () {
     mostrarTexto("lblValorIVA", valorIVA);
     let valorTotal = calcularTotal(valorSubtotal,valorDescuento,valorIVA);
     mostrarTexto("lblTotal", valorTotal);
-
-
-    //12. Mostrar un resumen en el componente lblResumen, si no existe debe agregarlo
-    /*
-        Ejemplo: 
-            Valor a pagar por 20 cerveza corona con 10% de descuento: USD 48.75
-        Si funciona, hacer un commit
-    */
+    mostrarTexto("lblResumen", "Valor a pagar por " + cantidad +" "+ nombreProducto + "   con " + porcentajeDescuento
+        + "% de descuento: USD " + valorTotal);
 
 }
 limpiar = function () {
