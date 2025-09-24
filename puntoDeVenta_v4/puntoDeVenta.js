19de952bb8f0372f7cd9e63c03333d803115fd87
luisjimenezgx@gmai.com <luisjimenezgx@gmail.com>
2025-09-24 16:37:03 -0400

    Recupera las variables con utilitaio y prueba Calculo Subtotal ok

---
+++ puntoDeVenta_v4/puntoDeVenta.js
@@ -0,0 +1,76 @@
+calcularValorTotal = function () {
+    //variables para recuperar los valores de las cajas de texto
+    let nombreProducto = recuperarTexto("txtProducto");
+    let precioProducto = recuperarFloat("txtPrecio"); // SE UTILIZA PARA RECUPERAR EL PRECIO COMO FLOAT
+    let cantidad = recuperarInt("txtCantidad"); // SE UTILIZA PARA RECUPERAR LA CANTIDAD COMO INT
+    let porcentajeDescuento = recuperarInt("txtPorcentajeDescuento");
+    
+    //variables para almacenar los retornos de las funciones
+    let valorSubtotal = calcularSubtotal(precioProducto,cantidad);
+    mostrarTexto("lblSubtotal", valorSubtotal);
+    let valorDescuento = (valorSubtotal * (porcentajeDescuento/100));
+    let valorIVA = ((valorSubtotal - valorDescuento) * 0.12);
+    let valorTotal = valorSubtotal - valorDescuento + valorIVA;
+
+
+    //6. Invocar a calcularValorDescuento y lo que devuelve guardar en la variable valorDescuento
+    //7. Mostrar el resultado en el componente lblDescuento
+    /*
+        Caso de prueba: 
+            - cantidad: 10 
+            - precioProducto: 5.4  
+            - descuento: 10
+            - Descuento esperado: 5.4
+        Si el caso de prueba es exitoso, hacer un commit
+     */
+    //8. Invocar a calcularIVA y lo que devuelve guardar en la variable valorIVA
+    // El IVA debe calcularse sobre el valor del subtotal menos el descuento
+    //9. Mostrar el resultado en el componente lblValorIVA    
+        /*
+            Caso de prueba: 
+                - cantidad: 10 
+                - precioProducto: 5.4  
+                - descuento: 10
+
+                    - valorSubtotal: 54
+                    - descuento:5.4
+                    - valorSubtotal 
+                    - descuento: 48.6
+
+                IVA esperado: 5.832
+
+            Si el caso de prueba es exitoso, hacer un commit
+        */
+    //10. Invocar a calcularTotal y lo que devuelve guardar en la variable valorTotal
+    //11. Mostrar el resultado en el componente lblTotal
+    /*
+        Caso de prueba: 
+            - cantidad: 10
+            - precioProducto: 5.4 
+            - descuento: 10
+
+                --valorSubtotal: 5.4
+                --descuento: 5.4
+                --IVA: 5.832
+
+                Total esperado: 54.432
+
+                Si el caso de prueba es exitoso, hacer un commit
+       */
+            
+    //12. Mostrar un resumen en el componente lblResumen, si no existe debe agregarlo
+    /*
+        Ejemplo: 
+            Valor a pagar por 20 cerveza corona con 10% de descuento: USD 48.75
+        Si funciona, hacer un commit
+    */
+
+}
+limpiar = function () {
+    /*
+        Dejar todas las cajas de texto con el valor cadena vacía, 0 ó 0.0 según el tipo de dato
+        Dejar todos los textos de los montos con el valor 0.0
+        Si funciona, hacer un commit
+     */
+}
+/* SI TODO FUNCIONA, HACER UN PUSH */
---
+++ puntoDeVenta_v4/serviciosPuntoDeVenta.js
@@ -0,0 +1,4 @@
+calcularSubtotal = function(precio,cantidad){
+    let subtotal = precio*cantidad;
+    return subtotal;
+}
