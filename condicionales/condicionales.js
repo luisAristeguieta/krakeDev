bfe11a6f9f20ec87e18be582bf3f92e317a6c082
luisjimenezgx@gmai.com <luisjimenezgx@gmail.com>
2025-09-29 16:55:36 -0400

    Crear archivo y Testea funcion calcularTasaInteres Ok

---
+++ condicionales/condicionales.js
@@ -0,0 +1,19 @@
+// Ejercicio # 1 crear funcion calcularTasaInteres 
+// Que hace: indica el valor de la tasa de interes dependiendo del valor anual de la empresa
+// Recibe: El valor Anual de la empresa.
+// Retorna = Valor tasa a ortogar
+calcularTasaInteres = function(ingresoAnual){
+    let valorTasa;
+        if (ingresoAnual < 300000){
+            valorTasa = 16;
+        } else if (ingresoAnual >= 300000 && ingresoAnual < 500000){
+            valorTasa = 15;
+        } else if (ingresoAnual >= 500000 && ingresoAnual < 1000000){
+            valorTasa = 14;
+        } else if (ingresoAnual >= 1000000 && ingresoAnual < 2000000){
+            valorTasa = 13;
+        }   else {
+            valorTasa = 12;
+        }
+    return valorTasa
+}
