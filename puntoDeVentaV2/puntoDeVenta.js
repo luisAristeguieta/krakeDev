4379c98bc1a0d39a0e67522e89274aa3ccd9524a
luisjimenezgx@gmai.com <luisjimenezgx@gmail.com>
2025-10-01 17:23:21 -0400

    Funciones con validaciones se anaden y prueban funcionamiento individual

--- puntoDeVentaV2/puntoDeVenta.js
+++ puntoDeVentaV2/puntoDeVenta.js
@@ -1,8 +1,11 @@
 calcularValorTotal = function () {
     //variables para recuperar los valores de las cajas de texto
     let nombreProducto = recuperarTexto("txtProducto");
+    esProductoValido(nombreProducto);
+   let cantidad = recuperarInt("txtCantidad"); // SE UTILIZA PARA RECUPERAR LA CANTIDAD COMO INT
+    esCantidadValida(cantidad);
     let precioProducto = recuperarFloat("txtPrecio"); // SE UTILIZA PARA RECUPERAR EL PRECIO COMO FLOAT
-    let cantidad = recuperarInt("txtCantidad"); // SE UTILIZA PARA RECUPERAR LA CANTIDAD COMO INT
+    esPrecioValido(precioProducto);
     //let porcentajeDescuento = calcularDescuentoPorVolumen(); Se anula este input 
     
     //variables para almacenar los retornos de las funciones
@@ -18,6 +21,9 @@ calcularValorTotal = function () {
     //mostrarTexto("lblResumen", "Valor a pagar por " + cantidad +" "+ nombreProducto + "   con $ " + valorDescuento
     //   + " de descuento: USD " + valorTotal);
 }
+
+
+
 limpiar = function () {
     mostrarTextoEnCaja("txtProducto","");
     mostrarTextoEnCaja("txtPrecio","0.0");
@@ -28,3 +34,67 @@ limpiar = function () {
     mostrarTexto("lblIva", "0.0");
     mostrarTexto("lblTotal", "0.0");
 }
+
+esProductoValido = function(nombreProducto){
+    if (nombreProducto == "") {
+        mostrarTexto("lblError1", "Campo Obligatorio");
+        existeError = true;
+    } else  if (nombreProducto.length > 10) {
+        mostrarTexto("lblError1", "Supera mas de 10 caracteres");
+        existeError = true;
+    } else {
+        mostrarTexto("lblError1", "");
+    }
+}
+
+esCantidadValida = function(cantidad){
+    if (isNaN(cantidad)) {
+        mostrarTexto("lblError2", "Campo Obligatorio");
+        existeError = true;
+    } else  if (cantidad < 0 || cantidad > 100) {
+        mostrarTexto("lblError2", "Cantidad Invalida");
+        existeError = true;
+    } else {
+        mostrarTexto("lblError2", "");
+    }
+}
+
+esPrecioValido = function(precioProducto){
+    if (isNaN(precioProducto)) {
+        mostrarTexto("lblError3", "Campo Obligatorio");
+        existeError = true;
+    } else  if (precioProducto < 0 || precioProducto > 50) {
+        mostrarTexto("lblError3", "Precio Invalido");
+        existeError = true;
+    } else {
+        mostrarTexto("lblError3", "");
+    } 
+}
+
+
+
+/*
+
+    nota1 = recuperarFloat("txtNota1");
+    if (isNaN(nota1)) {
+        mostrarTexto("lblError1", "Debe Ingresar un numero");
+        existeError = true;
+    } else {
+        mostrarTexto("lblError1", "");
+    }
+
+    
+esnotaValida = function (nota, idComponente) {
+        if (isNaN(nota)) {
+            mostrarTexto(idComponente, "Debe Ingresar un numero");
+            return false;
+        } else {
+            if (nota>=0 && nota <=10){
+            mostrarTexto(idComponente,"");
+            return true;
+            } else {
+                mostrarTexto(idComponente,"El numero debe estar entre 0 y 10")
+            }
+        }
+}
+*/
