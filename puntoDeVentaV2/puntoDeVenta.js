aa4cb780878a1327890c05b67ab1137a451394fd
luisjimenezgx@gmai.com <luisjimenezgx@gmail.com>
2025-10-01 17:37:00 -0400

    Se agrega condicion que debe cumplir con todos los campos validos para realizar el calculo

--- puntoDeVentaV2/puntoDeVenta.js
+++ puntoDeVentaV2/puntoDeVenta.js
@@ -1,23 +1,23 @@
 calcularValorTotal = function () {
     //variables para recuperar los valores de las cajas de texto
-    let nombreProducto = recuperarTexto("txtProducto");
-    esProductoValido(nombreProducto);
-   let cantidad = recuperarInt("txtCantidad"); // SE UTILIZA PARA RECUPERAR LA CANTIDAD COMO INT
-    esCantidadValida(cantidad);
-    let precioProducto = recuperarFloat("txtPrecio"); // SE UTILIZA PARA RECUPERAR EL PRECIO COMO FLOAT
-    esPrecioValido(precioProducto);
-    //let porcentajeDescuento = calcularDescuentoPorVolumen(); Se anula este input 
-    
+let nombreProducto = recuperarTexto("txtProducto");
+let cantidad = recuperarInt("txtCantidad"); // SE UTILIZA PARA RECUPERAR LA CANTIDAD COMO INT
+let precioProducto = recuperarFloat("txtPrecio"); // SE UTILIZA PARA RECUPERAR EL PRECIO COMO FLOAT
+
+    if(esProductoValido(nombreProducto) == false & 
+    esCantidadValida(cantidad)== false  & 
+    esPrecioValido(precioProducto) == false ){ 
     //variables para almacenar los retornos de las funciones
     let valorSubtotal = calcularSubtotal(precioProducto,cantidad);
     mostrarTexto("lblSubtotal", valorSubtotal);
     let valorDescuento = calcularDescuentoPorVolumen(valorSubtotal, cantidad);
-    mostrarTexto("lblDescuento", valorDescuento);
+    mostrarTexto("lblDescuento", valorDescuento.toFixed(2));
     let valorSubtotalConDescuento = valorSubtotal - valorDescuento;
     let valorIVA = calcularIva(valorSubtotalConDescuento);
-    mostrarTexto("lblIva", valorIVA);
+    mostrarTexto("lblIva", valorIVA.toFixed(2));
     let valorTotal = calcularTotal(valorSubtotal,valorDescuento,valorIVA);
-    mostrarTexto("lblTotal", valorTotal);
+    mostrarTexto("lblTotal", valorTotal.toFixed(2));
+    }
     //mostrarTexto("lblResumen", "Valor a pagar por " + cantidad +" "+ nombreProducto + "   con $ " + valorDescuento
     //   + " de descuento: USD " + valorTotal);
 }
@@ -28,7 +28,6 @@ limpiar = function () {
     mostrarTextoEnCaja("txtProducto","");
     mostrarTextoEnCaja("txtPrecio","0.0");
     mostrarTextoEnCaja("txtCantidad","0");
-    mostrarTextoEnCaja("txtPorcentajeDescuento","0");
     mostrarTexto("lblSubtotal", "0.0");
     mostrarTexto("lblDescuento", "0.0");
     mostrarTexto("lblIva", "0.0");
@@ -38,63 +37,38 @@ limpiar = function () {
 esProductoValido = function(nombreProducto){
     if (nombreProducto == "") {
         mostrarTexto("lblError1", "Campo Obligatorio");
-        existeError = true;
+        return true;
     } else  if (nombreProducto.length > 10) {
         mostrarTexto("lblError1", "Supera mas de 10 caracteres");
-        existeError = true;
+        return true;
     } else {
         mostrarTexto("lblError1", "");
+        return false;
     }
 }
 
 esCantidadValida = function(cantidad){
     if (isNaN(cantidad)) {
         mostrarTexto("lblError2", "Campo Obligatorio");
-        existeError = true;
+        return true;
     } else  if (cantidad < 0 || cantidad > 100) {
         mostrarTexto("lblError2", "Cantidad Invalida");
-        existeError = true;
+        return true;
     } else {
         mostrarTexto("lblError2", "");
+        return false;
     }
 }
 
 esPrecioValido = function(precioProducto){
     if (isNaN(precioProducto)) {
         mostrarTexto("lblError3", "Campo Obligatorio");
-        existeError = true;
+        return true;
     } else  if (precioProducto < 0 || precioProducto > 50) {
         mostrarTexto("lblError3", "Precio Invalido");
-        existeError = true;
+        return true;
     } else {
         mostrarTexto("lblError3", "");
+        return false;
     } 
 }
-
-
-
-/*
-
-    nota1 = recuperarFloat("txtNota1");
-    if (isNaN(nota1)) {
-        mostrarTexto("lblError1", "Debe Ingresar un numero");
-        existeError = true;
-    } else {
-        mostrarTexto("lblError1", "");
-    }
-
-    
-esnotaValida = function (nota, idComponente) {
-        if (isNaN(nota)) {
-            mostrarTexto(idComponente, "Debe Ingresar un numero");
-            return false;
-        } else {
-            if (nota>=0 && nota <=10){
-            mostrarTexto(idComponente,"");
-            return true;
-            } else {
-                mostrarTexto(idComponente,"El numero debe estar entre 0 y 10")
-            }
-        }
-}
-*/
