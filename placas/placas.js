3888723ca2e594ea76a161cded2310be008ef1e8
luisjimenezgx@gmai.com <luisjimenezgx@gmail.com>
2025-10-03 16:22:21 -0400

    Se realiza correccion que si algun parametro no es valido de la caja de texto, no invoca obtener provincia, tipo de vehiculo y dia de pico y placa, tambien borra mensajes si no se cumple la validacion

--- placas/placas.js
+++ placas/placas.js
@@ -3,27 +3,26 @@ validarPlaca = function(){
     let erroresEstructura = validarEstructura(placa);
     if (erroresEstructura == null){
         mostrarTexto("lblResultado", "ESTRUCTURA VALIDA");
+        
+        let nombreProvincia = obtenerProvincia(placa);
+        if (nombreProvincia == null){
+        mostrarTexto("lblProvincia", "Provincia Inconrrecta");
+        } else {
+        mostrarTexto("lblProvincia", "La placa pertenece a la provincia de: " + nombreProvincia);
+        }
+
+        let tipoVehiculo = obtenerTipoVehiculo(placa); 
+        mostrarTexto("lblTipoVehiculo", "Es tipo: " + tipoVehiculo);
+        let diaPicoYPlaca = obtenerDiaPicoYPlaca(placa);
+        mostrarTexto("lblPicoYPlaca", "El dia pico y placa es: " + diaPicoYPlaca);
+        mostrarTexto("lblError", "")
+        
     } else {
         mostrarTexto("lblResultado", "ESTRUCTURA INCORRECTA ");
-        mostrarTexto("lblError", erroresEstructura)
+        mostrarTexto("lblError", erroresEstructura);
+        mostrarTexto("lblProvincia", "");
+        mostrarTexto("lblTipoVehiculo", "");
+        mostrarTexto("lblPicoYPlaca", "");
     }
-    let nombreProvincia = obtenerProvincia(placa);
-    if (nombreProvincia == null){
-        mostrarTexto("lblProvincia", "Provincia Inconrrecta");
-    } else {
-        mostrarTexto("lblProvincia", "La placa pertenece a la provincia de: " + nombreProvincia);
-    }
-
-    let tipoVehiculo = obtenerTipoVehiculo(placa); 
-    mostrarTexto("lblTipoVehiculo", "Es tipo: " + tipoVehiculo);
-    /*
-    if (tipoVehiculo == null){ // Esta condicion esta adicionada sin proposito ya que no existe ningun vehiculo 
-    con etse parametro siendo nulo. En el segundo caractere de la placa no se excluye ninguna letra. 
-        mostrarTexto("lblTipoVehiculo", "Tipo de Vehiculo Incorrecto");
-    } else {
-        mostrarTexto("lblTipoVehiculo", "Es tipo: " + tipoVehiculo);
-    }*/
-
-    let diaPicoYPlaca = obtenerDiaPicoYPlaca(placa);
-    mostrarTexto("lblPicoYPlaca", "El dia pico y placa es: " + diaPicoYPlaca);
+    
 }
