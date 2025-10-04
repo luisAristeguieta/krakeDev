63595a5ae9d0f4b3e82bc7ce2f4680a0332145d5
luisjimenezgx@gmai.com <luisjimenezgx@gmail.com>
2025-10-04 16:41:29 -0400

    Se agrega boton limpar y condiciones de ganar o perder el juego, prueba ok

--- juego/juego.js
+++ juego/juego.js
@@ -8,6 +8,7 @@ let elemento = generarElemento();
 mostrarImagen ("img",generarRuta (elemento));
 let ganador = determinarGanador(seleccionado,elemento);
 mostrarTexto ("lblComputador","El Computador eligio:");
+mostrarTexto("lblResultadoFinal", "");
 
 if (ganador == 0){
     mostrarTexto("lblResultado", "Empate");
@@ -16,10 +17,41 @@ if (ganador == 0){
     mostrarTexto("lblResultado", "Ganaste la Partida");
     puntosUsuarios = puntosUsuarios + 1;
     mostrarTexto("lblUsuario", puntosUsuarios);
+    if(puntosUsuarios == 5){
+        puntosUsuarios = 0;
+        mostrarTexto("lblResultadoFinal", "HAS GANADO EL JUEGO");
+        mostrarTexto("lblUsuario", "");
+        mostrarTexto("lblComputador2", "")
+        mostrarImagen ("img","");
+        mostrarTexto("lblResultado", "");
+    }
+    
 } else {
     mostrarTexto("lblResultado", "Perdiste la Partida");
     puntosComputador = puntosComputador + 1;
     mostrarTexto("lblComputador2", puntosComputador);
+    if(puntosComputador == 5){
+        puntosComputador = 0;
+        mostrarTexto("lblResultadoFinal", "HAS PERDIDO EL JUEGO")
+        mostrarTexto("lblUsuario", "");
+        mostrarTexto("lblComputador2", "")
+        mostrarImagen ("img","");
+        mostrarTexto("lblResultado", "");
+    }
+    
 }
+
+}
+
+nuevaPartida = function(){
+    let puntosUsuarios = 0;
+    let puntosComputador = 0;
+    mostrarImagen ("img","");
+    mostrarTexto("lblResultado", "");
+    mostrarTexto("lblUsuario", "")
+    mostrarTexto("lblComputador2", "")
+    mostrarTexto("lblResultadoFinal", "");
+    mostrarTexto("lblUsuario", "");
+    mostrarTexto("lblComputador2", "");
 
 }
