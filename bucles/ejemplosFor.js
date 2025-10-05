00eb047c691b472968cfede643a43818fb537f7f
luisjimenezgx@gmai.com <luisjimenezgx@gmail.com>
2025-10-05 13:08:18 -0400

    Cargan archivo de ejercicios explicados en videos

---
+++ bucles/ejemplosFor.js
@@ -0,0 +1,38 @@
+mostrarNumeros = function(){
+    console.log("Antes del for");
+for (let i=0;i<4;i++){
+    console.log(i);
+}
+    console.log("Despues del for");
+}
+
+mostrarNumeros2 = function(){
+    console.log("Antes del for");
+for (let indice=1;indice<=5;indice++){
+    console.log(indice);
+}
+    console.log("Despues del for");
+}
+
+mostrarPares = function(){
+    console.log("Antes del for");
+for(let x=2; x<=10 ; x+=2){
+    console.log(x)
+}
+    console.log("Despues del for");
+}
+
+mostrarInverso = function(){
+    console.log("Antes del for");
+for(let i=10 ; i>=0 ; i--){
+    console.log(i)
+}
+    console.log("Despues del for");
+}
+
+mostrarNasa = function(){
+    for(let i=0 ; i<=100 ; i+=10){
+        console.log("Hackeanado en " + i + "%")
+    }
+       console.log(" Ha sido hackeada") 
+}
