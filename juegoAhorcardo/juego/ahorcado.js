06d2a25d606e9d986adc8520b271cced29c8f8ae
luisjimenezgx@gmai.com <luisjimenezgx@gmail.com>
2025-10-07 23:52:34 -0400

    Se agregan restante de funciones y se prueban ok

--- juegoAhorcardo/juego/ahorcado.js
+++ juegoAhorcardo/juego/ahorcado.js
@@ -68,24 +68,56 @@ mostrarLetra = function(letra,posicion){
 
 validar = function(letra){
 
-letrasEncotradas = 0;
+letrasEncontradas = 0;
 
 for (i = 0 ; i < palabraSecreta.length ; i++ ){
     let caracter = palabraSecreta.charAt(i);
     if (caracter == letra){
         mostrarLetra(letra,i);
-        letrasEncotradas++;
+        letrasEncontradas++;
+        coincidencias +=1;
     } 
 }
-
+if (letrasEncontradas == 0) {
+        alert("La letra No es parte de la palabra");
+        errores+=1;
+        mostrarAhorcado();
 }
+}
+
+let intentos = 0;
+let coincidencias = 0;
+let errores = 0;
 
 ingresarLetra = function(){
     let letra = recuperarTexto ("txtLetra");
 
+    if (letra.length == 0){
+        alert("Debe ingresar una letra Mayuscula");
+        return;
+    }
+
+    intentos += 1;
+
     if (esMayuscula(letra)){
         validar(letra);
+
+        if (coincidencias == 5){
+            alert("Ha Ganado")
+            mostrarImagen("ahorcadoImagen","./ganador.gif")
+        } else if (intentos == 10) {
+            alert("Ha Perdido")
+            mostrarImagen("ahorcadoImagen","./gameOver.gif")
+        }
+        
     } else {
         alert("SOLO SE ACEPTAN LETRAS MAYUSCULAS")
     }
 }
+
+mostrarAhorcado = function(){
+    mostrarImagen("ahorcadoImagen","./Ahorcado_0" + errores + ".png")
+}
+
+
+
